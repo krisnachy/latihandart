62d1ecb93a40718263fd255d193457f22335952c
krisnachy <cahyayuana@gmail.com>
2021-08-01 17:19:19 +0700

    Add switch case

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -15,11 +15,27 @@ void main(List<String> arguments) {
   // }
 
   //Penyingkatan if else jika menentukan nilai output
-  String output;
-  output = (number > 0) ? "positif" : "negatif atau nol";
+  // String output;
+  // output = (number > 0) ? "positif" : "negatif atau nol";
 
-  print(output);
+  // print(output);
 
   //atau
-  print((number > 0) ? "positif" : "negatif atau nol");
+  // print((number > 0) ? "positif" : "negatif atau nol");
+
+  //Menggunakan switch
+  switch (number) {
+    case 0:
+      print('nol');
+      break;
+    case 1:
+      print(number);
+      print('satu');
+      break;
+    case 2:
+      print('dua');
+      break;
+    default:
+      print('Bilangan lain');
+  }
 }
