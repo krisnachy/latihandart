7146e6d2fec72ef78ffa47316e0b0b5db66b3d89
krisnachy <cahyayuana@gmail.com>
2021-08-01 18:51:52 +0700

    Function new

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,26 +1,45 @@
+import 'dart:ffi';
 import 'dart:io';
 
+double luasSegiEmpat(double panjang, double lebar) {
+  double hasil;
+  hasil = panjang * lebar;
+  return hasil;
+}
+
+//Jika tidak mengembalikan sesuatu maka menggunakan void
+void nama() {
+  print("Krisna");
+}
+
+//Penggunaan optional parameter yang mana parameter boleh diisi atau tidak
+String pesan(String from, String message, {String to, String via}) {
+  return from +
+      ' say ' +
+      message +
+      ((to != null) ? ' to ' + to : "") +
+      ((via != null) ? ' via ' + via : "");
+}
+
+String pesan2(String from, String message, [String to, String via]) {
+  return from +
+      ' say ' +
+      message +
+      ((to != null) ? ' to ' + to : "") +
+      ((via != null) ? ' via ' + via : "");
+}
+
 void main(List<String> arguments) {
-  // for (int count = 0; count < 5; count = count + 1) {
-  //   print('Hai');
-  // }
+  // double p, l, luas;
 
-  //Penggunaan while
-  // int i = 0;
+  // p = double.tryParse(stdin.readLineSync());
+  // l = double.tryParse(stdin.readLineSync());
 
-  // while (i < 5) {
-  //   print('Hai');
-  //   // i++;
-  //   //atau
-  //   i += 1;
-  // }
+  // luas = luasSegiEmpat(p, l);
+  // print(luas);
 
-  int a, b, c, d;
-  a = 10;
-  b = a++;
-  print(a.toString() + " - " + b.toString());
+  // nama();
 
-  c = 10;
-  d = ++c;
-  print(c.toString() + " - " + d.toString());
+  print(pesan("Johnny", "Sins", to: "You", via: "PH"));
+  print(pesan2('Me', 'Hi', 'You'));
 }
