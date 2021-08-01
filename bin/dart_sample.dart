a33b5589f127b59758cba37966b8ef3bf5e00da0
krisnachy <cahyayuana@gmail.com>
2021-08-01 18:02:13 +0700

    Add while loop

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  for (int count = 0; count < 5; count = count + 1) {
+  // for (int count = 0; count < 5; count = count + 1) {
+  //   print('Hai');
+  // }
+
+  //Penggunaan while
+  int i = 0;
+
+  while (i < 5) {
     print('Hai');
+    i++;
   }
 }
