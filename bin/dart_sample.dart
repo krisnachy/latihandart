ff77b8d5285e2c33331118eb3056f12faecf3c8b
krisnachy <cahyayuana@gmail.com>
2021-11-26 13:16:45 +0700

    Latihan menggunakan constructor

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,41 +1,12 @@
 import 'dart:io';
+import 'person.dart';
+import 'student.dart';
 
 void main(List<String> arguments) {
-  // String input = stdin.readLineSync();
-  // print(input);
+  Person p = Person('Joko');
+  Person q = Person();
+  // p.name = 'Joko';
 
-  int number = int.tryParse(stdin.readLineSync());
-
-  // if (number > 0) {
-  //   print('positif');
-  // } else if (number < 0) {
-  //   print('negatif');
-  // } else {
-  //   print('nol');
-  // }
-
-  //Penyingkatan if else jika menentukan nilai output
-  // String output;
-  // output = (number > 0) ? "positif" : "negatif atau nol";
-
-  // print(output);
-
-  //atau
-  // print((number > 0) ? "positif" : "negatif atau nol");
-
-  //Menggunakan switch
-  switch (number) {
-    case 0:
-      print('nol');
-      break;
-    case 1:
-      print(number);
-      print('satu');
-      break;
-    case 2:
-      print('dua');
-      break;
-    default:
-      print('Bilangan lain');
-  }
+  print(p.name);
+  print(q.name);
 }
---
+++ bin/person.dart
@@ -0,0 +1,15 @@
+class Person {
+  String name;
+
+  //constructor
+  // Person() {
+  //   print("Sedang membuat objek person");
+  //   name = "no name";
+  // }
+
+  // Jika tidak memberikan [] maka pada main wajib memasukkan parameter
+  Person([String name = 'no name']) {
+    print('constructor person');
+    this.name = name;
+  }
+}
