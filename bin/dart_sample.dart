b19edaf9bc0e92d608a9c385abfbcdd7eebed7b4
krisnachy <cahyayuana@gmail.com>
2021-12-13 17:04:36 +0700

    Using underscore to ignore parameter

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,3 +1,23 @@
-import 'dart:io';
+import 'package:dart_sample/ignore_parameter.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  // var p = Person('David');
+  // p.takeARest();
+
+  // var q = Person('Dave', doingHobby: daveHobby);
+  // q.takeARest();
+
+  var r = Person('Dove', doingHobby: (String name) {
+    print('$name is running');
+  });
+  r.takeARest();
+
+  var s = Person('Deve', doingHobby: (_) {
+    print('Is using underscore');
+  });
+  s.takeARest();
+}
+
+// void daveHobby(String name) {
+//   print('$name is swimming');
+// }
