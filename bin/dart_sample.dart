f0b8806d1d902453ca27998d18a8d5d563d7ade3
krisnachy <cahyayuana@gmail.com>
2021-12-14 14:50:26 +0700

    Add async, await, future

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,5 +1,39 @@
 import 'dart:io';
 
-void main(List<String> arguments) {}
+//untuk menggunakan await harus menggunakan async pada methodnya
+void main(List<String> arguments) async {
+  var p = Person();
 
-class Person {}
+  // print('job 1');
+  // print('job 2');
+  // await p.getDataAsync();
+  // print('job 3 : ' + p.name);
+  // print('job 4');
+
+  //Jika ingin hanya job 3 yang delay maka dapat menggunakan perintah seperti di bawah tanpa menggunakan await
+  print('job 1');
+  print('job 2');
+  //karena kemablian void(kosong) maka kembalian pada value tidak ada
+  p.getDataAsync().then((_) {
+    print('job 3 : ' + p.name);
+  });
+  print('job 4');
+}
+
+class Person {
+  String name = 'default name';
+
+  void getData() {
+    name = 'Joko';
+    print('get data [done]');
+  }
+
+  //future menunjukkan bahwa ketika get data hasilnya tidak langsung kembali
+  //tipe kembalian dapat diganti sesuai kebutuhan tidak harus void
+  Future<void> getDataAsync() async {
+    //await berfungsi sebagai penunggu method agar selesai
+    await Future.delayed(Duration(seconds: 3));
+    name = 'Joko';
+    print('get data [done]');
+  }
+}
