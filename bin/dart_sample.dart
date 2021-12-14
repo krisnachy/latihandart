ee985d0f57a84d630ad66991a4e6cf370d1285d2
krisnachy <cahyayuana@gmail.com>
2021-12-14 15:36:05 +0700

    generic

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,5 +1,13 @@
-import 'dart:io';
+import 'package:dart_sample/datetime_secure_box.dart';
+import 'package:dart_sample/int_secure_box.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var box = IntSecureBox(100, '123');
 
-class Person {}
+  print(box.getData('123').toString());
+  //Jika salah
+  print(box.getData('321').toString());
+
+  var dateBox = DateTimeSecureBox(DateTime.now(), '456');
+  print(dateBox.getData('456'));
+}
