61264fdff8438efb615cb91cfd7b76213a65fe86
krisnachy <cahyayuana@gmail.com>
2021-12-20 16:50:37 +0700

    Add Custom Sort and For Each

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,5 +1,51 @@
 import 'dart:io';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  List<Person> persons = [
+    Person('Administrator', 32),
+    Person('User', 18),
+    Person('User', 43),
+    Person('User', 14),
+    Person('User', 33),
+    Person('User', 21),
+    Person('Merchant', 29)
+  ];
 
-class Person {}
+  // persons.sort((p1, p2) => p1.age - p2.age);
+  // persons.sort((p1, p2) => p1.role.compareTo(p2.role));
+  // persons.sort((p1, p2) {
+  //   if(p1.role.compareTo(p2.role) != 0) {
+  //     return p1.role.compareTo(p2.role);
+  //   } else {
+  //     return p1.age.compareTo(p2.age);
+  //   }
+  // });
+  persons.sort((p1, p2) {
+    if (p1.roleWeight - p2.roleWeight != 0) {
+      return p1.roleWeight - p2.roleWeight;
+    } else {
+      return p1.age.compareTo(p2.age);
+    }
+  });
+
+  persons.forEach((element) {
+    print(element.role + ' - ' + element.age.toString());
+  });
+}
+
+class Person {
+  final String role;
+  final int age;
+
+  Person(this.role, this.age);
+
+  int get roleWeight {
+    switch(role) {
+      case 'Merchant': return 1;
+      break;
+      case 'User': return 2;
+      break;
+      default: return 3;
+    }
+  }
+}
