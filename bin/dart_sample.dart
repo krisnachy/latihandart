8ccdfae2666a0f3ffe9958d4b8385e1d908d734a
krisnachy <cahyayuana@gmail.com>
2021-12-20 16:17:50 +0700

    Add required, assertion dan try catch

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,5 +1,26 @@
-import 'dart:io';
+import 'package:meta/meta.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  Person p;
+  // p = Person(name: null);
 
-class Person {}
+  //try catch digunakan untuk kondisi yang berpotensi menimbulkan error, sehingga tidak menghambat kode lain untuk berjalan
+  try {
+    p = Person(name: null);
+  } catch (e) {
+    print(e);
+  }
+
+  print('Hello World');
+}
+
+class Person {
+  final String name;
+  final int age;
+
+  //required membuat inisialisasi objek otomatis memasukkan parameter
+  Person({@required this.name, this.age = 0}) {
+    //assert membuat kondisi objek harus memenuhi syarat
+    assert(name != null, "You must give the Person name");
+  }
+}
