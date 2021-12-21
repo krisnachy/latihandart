5cb3851cb0d3698fdce6151b7649dd6935c12861
krisnachy <cahyayuana@gmail.com>
2021-12-21 17:06:52 +0700

    Overriding first

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  // String a = "Hello";
+  // String b = "World";
 
-class Person {}
+  // print(a+b);
+  Hero a = Hero(10);
+  Hero b = Hero(20);
+
+  print((a+b).power);
+}
+
+class Hero {
+  final int power;
+
+  Hero(this.power);
+
+  Hero operator +(Hero other) {
+    return Hero(power + other.power);
+  }
+}
