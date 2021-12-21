e0c0fe862d6c590e2378bf617c2aef45e288844e
krisnachy <cahyayuana@gmail.com>
2021-12-21 17:22:54 +0700

    Add Overriding method

--- bin/dart_sample.dart
+++ bin/dart_sample.dart
@@ -8,7 +8,9 @@ void main(List<String> arguments) {
   var a = Hero(10);
   var b = Hero(20);
 
-  print((a+b).power);
+  // print((a+b).power);
+  // print(a == b);
+  print((a + 12).power);
 }
 
 class Hero {
@@ -16,7 +18,28 @@ class Hero {
 
   Hero(this.power);
 
-  Hero operator +(Hero other) {
-    return Hero(power + other.power);
+  // Hero operator +(Hero other) {
+  //   return Hero(power + other.power);
+  // }
+
+  Hero operator +(var other) {
+    if (other is Hero) {
+      return Hero(power + other.power);
+    } else if (other is int) {
+      return Hero(power + other);
+    } else {
+      return this;
+    }
+  }
+
+  @override
+  bool operator == (var other) {
+    if (other is Hero) {
+      if (other.power == power) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
